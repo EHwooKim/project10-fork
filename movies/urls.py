51199a8e622df341e30ad073f59da1561f709447
EHwooKim <ehwoo0707@naver.com>
2019-11-20 17:44:24 +0900

    TW | Add review crate, delete

--- movies/urls.py
+++ movies/urls.py
@@ -5,5 +5,7 @@ app_name = "movies"
 
 urlpatterns =[
     path('', views.index, name="index"),
-    path('<int:movie_pk>/', views.detail, name="detail")
+    path('<int:movie_pk>/', views.detail, name="detail"),
+    path('<int:movie_pk>/reviews/new/', views.new, name='new'),
+    path('<int:movie_pk>/reviews/<int:review_pk>/delete/', views.delete, name='delete'),
 ]
