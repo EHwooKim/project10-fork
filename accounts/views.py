323752ec6c27b4c6bacaf56ed72398d46359c599
ssshhh0402 <ssshhh0402@naver.com>
2019-11-20 14:14:49 +0900

    SB| AUTH_USER_MODEL 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from django.contrib.auth import get_user_model
 # Create your views here.
 def index(request):
-    pass
+    User = get_user_model()
+    users = User.objects.all()
+    context = {
+        'users': users
+    }
+    return render(request,'accounts/index.html', context)
+    
