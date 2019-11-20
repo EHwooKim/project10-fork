2bd94ac0ab231a33cdca2690eb7c5c443b227af1
ssshhh0402 <ssshhh0402@naver.com>
2019-11-20 15:45:58 +0900

    SB | accounts:index 완성

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,4 +4,4 @@ from django.contrib.auth.forms import UserCreationForm
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = get_user_model()
-        fields = ('username','password')
+        fields = ('username',)  
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
 from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 # Create your views here.
 def index(request):
     User = get_user_model()
@@ -8,4 +12,45 @@ def index(request):
         'users': users
     }
     return render(request,'accounts/index.html', context)
-    
+
+def log_in(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'accounts/form.html', context)
+
+def log_out(request):
+    auth_logout(request)
+    return redirect('accounts:index')
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('accounts:index')
+    if request.method =='POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('accounts:index')
+    else:
+        form = CustomUserCreationForm()
+    context= {
+        'form' : form
+    }
+    return render(request, 'accounts/form.html', context)
+
+def user(request, user_pk):
+    User = get_user_model()
+    user = get_object_or_404(User, pk=user_pk)
+    context=  {
+        'user' : user
+    }
+    return render(request, 'accounts/profile.html', context)
